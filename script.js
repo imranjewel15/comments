3626182e7c8ecfce1ba38e2049ce1a19de2a340e
MD IMRAN HOSSAIN <153748024+imranjewel15@users.noreply.github.com>
2024-09-27 01:23:29 +0600

    Add files via upload
    Liked Button Done

---
+++ script.js
@@ -0,0 +1,7 @@
+let like = document.getElementById('like');
+console.log(likeicon);
+function likeClick(){
+like.innerHTML='<i class="fa-solid fa-thumbs-up"></i> Liked';
+like.style.backgroundColor="#3a3b3c";
+}
+let commentBox = document.getElementById('commentBox');
